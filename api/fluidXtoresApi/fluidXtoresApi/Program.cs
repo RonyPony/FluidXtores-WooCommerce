5dd5d18495e5f33470aa3b5fe334b76ae834e3b0
Ronel Cruz <rocruz@apap.com.do>
2022-01-08 23:43:13 -0400

    adding user endpoint, still saving in memory

--- api/fluidXtoresApi/fluidXtoresApi/Program.cs
+++ api/fluidXtoresApi/fluidXtoresApi/Program.cs
@@ -1,8 +1,15 @@
+using fluidXtoresApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<userContext>(opt =>
+{
+    opt.UseInMemoryDatabase("users");
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
